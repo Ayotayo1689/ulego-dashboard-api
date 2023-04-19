2cff22e4f7d040c9c1cac62e3b734d9d4c29b6e7
Ayotayo <ayotayobadejo@gmail.com>
2023-04-18 14:20:05 -1100

    added new api links

--- routes/airtime.js
+++ routes/airtime.js
@@ -7,4 +7,5 @@ const airtimeModel = require('../model/airtime')
 router.get('/get/airtime/:pageNo', airtimeModel.allAirtime)
 router.get('/get/airtime-ref/:refNo', airtimeModel.airtimeRef) 
 
-module.exports = router 
+module.exports = router;
+
